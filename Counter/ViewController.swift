50650b56ca5d5795c71a56f81a780a6fd1bcb1f5
Stanislav <stas-shut@rambler.ru>
2023-08-28 12:32:07 +0300

    Добавлены UILabel и UIButton + констреинты, также дописан код со счётчиком для ДЗ

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var clickOnMeButton: UIButton!
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabel.text = "Значение счётчика: 0"
     }
 
-
+    @IBAction func touchToGetCount(_ sender: UIButton) {
+        count += 1
+        counterLabel.text = "Значение счётчика: \(count)"
+    }
+    
 }
 
